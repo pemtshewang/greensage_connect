db85a44503d39446cd352d19876b4ca9f25de47a
Deki Wngm <82018693+Deki2001@users.noreply.github.com>
2023-12-06 11:36:42 -0500

    changed

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { HomeStyles } from './styles/styles';
 import PasswordInput from './components/PasswordInput';
 import TextInput from './components/TextInput';
 import { NativeBaseProvider } from 'native-base';
-
+import CustomizedButton from './components/CustomizedButton';
 export default function App() {
   return (
     <NativeBaseProvider>
@@ -12,6 +12,7 @@ export default function App() {
         <Text>Working or</Text>
         <CustomStatusBar style='auto' />
         <PasswordInput width='50%' />
+        <CustomizedButton width ='30%' />
         <Text>Working or</Text>
         <TextInput placeholder='Email' width="1" />
       </View>
---
+++ src/screens/Auth/login.tsx
@@ -0,0 +1,10 @@
+import { TextInput, View  } from "react-native";
+import PasswordInput from "../../components/PasswordInput";
+
+function login() {
+    return <View>
+        <PasswordInput/>   
+    </View>
+}
+
+export default login;
