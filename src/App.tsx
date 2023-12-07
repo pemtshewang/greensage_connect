0b665c9ce528c4143062c3856f1345c542dd424b
Deki Wngm <82018693+Deki2001@users.noreply.github.com>
2023-12-06 19:44:10 -0500

    merged

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,11 @@ import { HomeStyles } from './styles/styles';
 import PasswordInput from './components/PasswordInput';
 import TextInput from './components/TextInput';
 import { NativeBaseProvider } from 'native-base';
-<<<<<<< HEAD
 import { Button } from 'native-base';
 import { ButtonStyle } from './styles/styles';
 import Register from './screens/Auth/register';
-=======
 import Login from './screens/Auth/login';
 
->>>>>>> 1f5f6e8 (added login form)
 export default function App() {
   return (
     <NativeBaseProvider>
