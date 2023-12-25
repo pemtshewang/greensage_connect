b59ca16bc642dd18875654463337d99afb709647
pemtshewang <pemtshewang500@gmail.com>
2023-12-25 13:09:09 +0600

    added WebSocketConnectionTestForm

---
+++ components/Forms/WebSocketConnectionForm.tsx
@@ -0,0 +1,15 @@
+import { View } from "native-base";
+import CustomModal from "../ui/Modal";
+
+export default function WSTestConnectionForm({ id,
+  showForm,
+  setShowForm
+}: {
+  id: string;
+}) {
+  return (
+    <CustomModal>
+
+    </CustomModal>
+  )
+}
